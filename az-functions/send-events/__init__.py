b24f118260b9d37424481afce5f24845a537a744
Rafal Naczelnik <rafal.naczelnik@gmail.com>
2023-08-28 21:40:05 +0200

    Creates __init__.py. It is file which runs in Azure Funcions.

---
+++ az-functions/send-events/__init__.py
@@ -0,0 +1,13 @@
+import asyncio
+import logging
+
+import azure.functions as func
+
+from .send import run
+from .event import event_to_send
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    asyncio.run(run(event=event_to_send))
