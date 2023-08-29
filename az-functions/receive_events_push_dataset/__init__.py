2bcf64df9590cf93c4cc667bf5ecba53763c677b
Rafal Naczelnik <rafal.naczelnik@gmail.com>
2023-08-29 16:48:27 +0200

    Added new funcionalities to receive_events_push_dataset fucnion: 1. Receive events from Event Hub. 2. Write each event as json file to blob container.

--- az-functions/receive_events_push_dataset/__init__.py
+++ az-functions/receive_events_push_dataset/__init__.py
@@ -1,10 +1,23 @@
+"""Get events from Event Hub, sink them to blob container and sent to PBI push dataset."""
+
 from typing import List
 import logging
+from datetime import datetime
 
 import azure.functions as func
 
+from .connection_config import container_pbi_push_dataset_raw_data
+
 
 def main(events: List[func.EventHubEvent]):
-     for event in events:
-         logging.info('Python EventHub trigger processed an event: %s',
-                         event.get_body().decode('utf-8'))
+    for event in events:
+        # Get event body
+        event_body = event.get_body().decode("utf-8")
+        logging.info(f"Python EventHub trigger processed an event: \n {event_body}")
+
+        # Sent event body to container in json format
+        container_pbi_push_dataset_raw_data.upload_blob(
+            name=f"export_{datetime.utcnow()}.json",
+            data=event_body,
+            blob_type="BlockBlob",
+        )
